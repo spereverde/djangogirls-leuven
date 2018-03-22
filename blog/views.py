972fead03ee6c55414d8e6a21a264cc0e0c82b6f
Kim Paulissen <kim.paulissen@kuleuven.be>
2018-03-22 11:58:46 +0100

    small template adjustments

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from django import template
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	# return render(request, 'blog/post_list.html', {'posts': posts})
-	return render(request, 'blog/post_kul.html', {'posts': posts})
+	return render(request, 'blog/post_list.html', {'posts': posts})
+	# return render(request, 'blog/post_kul.html', {'posts': posts})
