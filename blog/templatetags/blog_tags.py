bb565c580a032bb53ca7d8c46be82dd85cf416ee
Kim Paulissen <kim.paulissen@kuleuven.be>
2018-03-22 14:56:56 +0100

    test reading from webservice

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,4 @@
-# import requests
+import requests
 import datetime
 from django.utils import timezone
 from django import template
@@ -6,12 +6,26 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-# @register.simple_tag
-# def include_external(url):
-# 	include = requests.get(url)
-# 	include_safe = mark_safe(include.text)
-# 	return include_safe
+@register.simple_tag
+def include_external(url):
+	include = requests.get(url)
+	include_safe = mark_safe(include.text)
+	return include_safe
 
 @register.simple_tag
 def current_time(format_string):
     return datetime.datetime.now().strftime(format_string)
+
+@register.simple_tag
+def get_person():
+    url = 'https://webwsp.aps.kuleuven.be/esap/public/odata/sap/zh_person_srv/Persons?$format=json&$filter=userId%20eq%20%27u0010287%27'
+    # params = {'year': year, 'author': author}
+    r = requests.get(url)
+    # r = requests.get(url, params=params)
+    people = r.json()
+    # this returns a list of all people from the search done by the params:
+    # all_people = people['d']['results']
+    # this return only the first item from the list of people:
+    person_new = people['d']['results'][0]
+    # person_new = person['nr']
+    return person_new
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
 from django import template
+from .models import Post
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html', {'posts': posts})
-	# return render(request, 'blog/post_kul.html', {'posts': posts})
+	# return render(request, 'blog/post_list.html', {'posts': posts})
+	return render(request, 'blog/post_kul.html', {'posts': posts})
